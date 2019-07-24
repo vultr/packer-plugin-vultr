7aa9940cb56928eb308b5356ff064922336cd4a9
David Dymko <dymkod@gmail.com>
2019-07-24 07:26:50 -0400

    tests

---
+++ vultr/builder_acc_test.go
@@ -0,0 +1,35 @@
+package vultr
+
+import (
+	"os"
+	"testing"
+
+	builderT "github.com/hashicorp/packer/helper/builder/testing"
+)
+
+func TestBuilderAcc_basic(t *testing.T) {
+	builderT.Test(t, builderT.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		Builder:  &Builder{},
+		Template: testBuilderAccBasic,
+	})
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("VULTR_API_KEY"); v == "" {
+		t.Fatal("VULTR_API_KEY must be set for acceptance tests")
+	}
+}
+
+const testBuilderAccBasic = `
+{
+	"builders": [{
+		"type": "test",
+		"snapshot_description": "packer-test-snapshot",
+		"region_id":            "3",
+		"os_id":                "127",
+		"plan_id":              "401",
+		"ssh_username":         "root"
+	}]
+}
+`
