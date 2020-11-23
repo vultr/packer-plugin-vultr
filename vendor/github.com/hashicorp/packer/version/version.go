b5f1fcf7d28fe44275d3298fe434a1f5993130d0
David Dymko <dymkod@gmail.com>
2020-11-23 11:48:19 -0500

    dep updates

--- vendor/github.com/hashicorp/packer/version/version.go
+++ vendor/github.com/hashicorp/packer/version/version.go
@@ -3,13 +3,15 @@ package version
 import (
 	"bytes"
 	"fmt"
+
+	"github.com/hashicorp/go-version"
 )
 
 // The git commit that was compiled. This will be filled in by the compiler.
 var GitCommit string
 
 // The main version number that is being run at the moment.
-const Version = "1.6.4"
+const Version = "1.6.5"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
@@ -28,4 +30,21 @@ func FormattedVersion() string {
 	}
 
 	return versionString.String()
+}
+
+// SemVer is an instance of version.Version. This has the secondary
+// benefit of verifying during tests and init time that our version is a
+// proper semantic version, which should always be the case.
+var SemVer *version.Version
+
+func init() {
+	SemVer = version.Must(version.NewVersion(Version))
+}
+
+// String returns the complete version string, including prerelease
+func String() string {
+	if VersionPrerelease != "" {
+		return fmt.Sprintf("%s-%s", Version, VersionPrerelease)
+	}
+	return Version
 }
