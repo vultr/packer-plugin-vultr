5bdc2a8710b8518f2d0395f38b9375324bc6dd72
Michael Riley <mriley@vultr.com>
2023-04-05 14:58:46 -0400

    Remove vendored dependencies

--- vendor/github.com/hashicorp/packer-plugin-sdk/filelock/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-/*
-Package filelock makes it easy to create and check file locks for concurrent
-processes.
-*/
-package filelock
