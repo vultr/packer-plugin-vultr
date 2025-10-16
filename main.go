6ffc0de051e8d1497248ca49238545b9f6e2da9e
Carlos Mondragon <cmondragon@vultr.com>
2025-10-16 17:53:35 -0400

    Replace deprecated InitializePluginVersion function with NewPluginVersion

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ var (
 
 	// PluginVersion is used by the plugin set to allow Packer to recognize
 	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
+	PluginVersion = version.NewPluginVersion(Version, VersionPrerelease, "")
 )
 
 func main() {
