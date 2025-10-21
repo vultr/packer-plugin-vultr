f042ad7cae10e87da7c5795dec779e4f54fc52da
mondragonfx <35617339+mondragonfx@users.noreply.github.com>
2025-10-21 11:36:35 -0400

    Release v2.6.3 #patch

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	// Version is the main version number that is being run at the moment.
-	Version = "2.6.2"
+	Version = "2.6.3"
 
 	// VersionPrerelease is A pre-release marker for the Version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
